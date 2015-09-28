b13cdda2ac499c9f3b8977311d9e7a42a074b856
azarchs <azarchs@ac924af6-8399-422c-94d3-e5ec782a8fea>
2015-09-28 00:44:05 +0000

    Main window title.
    git-svn-id: svn+ssh://svn.code.sf.net/p/photocull/code@3 ac924af6-8399-422c-94d3-e5ec782a8fea

--- PhotoCull/Properties/AssemblyInfo.cs
+++ PhotoCull/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Adam Azarchs")]
 [assembly: AssemblyProduct("PhotoCull")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
